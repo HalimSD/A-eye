12b43241983b4b70d2aacaf4030abb5212cb825e
HalimSD <maingateway1@gmail.com>
2022-02-26 15:54:13 +0100

    add data generation functionality and training code for HAARCascade

--- ML-impl/cascade_classifier/data_preprocessing.py
+++ ML-impl/cascade_classifier/data_preprocessing.py
@@ -1,5 +1,5 @@
 # Downloads dogs_vs_cats dataset from Kaggle https://www.kaggle.com/c/dogs-vs-cats/data
-# Move each category into its own folder then convert them to grayscale colors and resize them to 100 * 100
+# Move each category into its own folder then convert the images to grayscale colors and resize them to 100 * 100
 # adding a descriptor file bg.txt for all negative files
 import os
 import shutil
@@ -7,8 +7,11 @@ import cv2
 
 
 source_dir = './data/train_data/train_data/train'
+negatives_path = './data/train_data/negatives'
+positives_path = './data/train_data/positives'
 cats_target_dir = './data/train_data/negatives/cats'
 dogs_target_dir = './data/train_data/negatives/dogs'
+
 
 files = os.listdir(source_dir)
 # cat_files = [os.remove(os.path.join(os.getcwd(), cat_file)) 
@@ -41,15 +44,36 @@ def resize_dog_files():
             print(str(e))
 
 def create_pos_n_neg():
-    for category in os.listdir('./data/train_data/negatives'):
+    for category in os.listdir(negatives_path):
             if "." not in category:
-                for img in os.listdir(os.path.join('./data/train_data/negatives', category)):
-                    if "." not in category:
-                        line = category+'/'+img+'\n'
-                        with open('bg.txt','a') as f:
-                            f.write(line)
+                for img in os.listdir(os.path.join(negatives_path, category)):
+                    try:
+                        if "." not in category:
+                            line = os.path.join(negatives_path, category)+'/'+img+'\n'
+                            with open('bg.txt','a') as f:
+                                f.write(line)
+                    except Exception as e:
+                        print(str(e))
 
+    for category in os.listdir(positives_path):
+        if "." not in category:
+                for image in os.listdir(os.path.join(positives_path, category)):
+                    try:
+                        if "." not in category:
+                            img = cv2.imread(os.path.join(os.path.join(positives_path, category), image),cv2.IMREAD_GRAYSCALE)
+                            resized_image = cv2.resize(img, (20, 20))
+                            cv2.imwrite(os.path.join(positives_path,os.path.join(category, image)),resized_image)
+                    except Exception as e:
+                        print(str(e))
 
 # resize_dog_files()
 # resize_cat_files()
 create_pos_n_neg()
+
+
+
+# COMMAND LINES TO GENERATE POSITIVE IMAGES FOR TRAINING BY PLACING THE POSITIVE IMAGE OVER NEGATIVE IMAGES
+
+# opencv_createsamples -img data/train_data/positives/watches/watch5050_copy.jpg -bg bg.txt -info data/train_data/positives/generated_positives/info.lst -pngoutput data/train_data/positives/generated_positives -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1950
+# opencv_createsamples -info data/train_data/positives/generated_positives/info.lst -num 1950 -w 20 -h 20 -vec positives.vec
+# opencv_traincascade -data data/train_data/training_cascade -vec positives.vec -bg bg.txt -numPos 1800 -numNeg 900 -numStages 10 -w 20 -h 20
