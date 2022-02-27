7aea4ad7d81352288704146e42c09b05ab86f5c9
HalimSD <maingateway1@gmail.com>
2022-02-27 19:24:24 +0100

    save extracted HOG features for SVM in pickle && code refactoring

--- ML-impl/SVM/data_preprocessing.py
+++ ML-impl/SVM/data_preprocessing.py
@@ -1,16 +1,20 @@
 # Data from https://www.kaggle.com/constantinwerner/human-detection-dataset 
 # HOG feature extractor: 1- preprocess imgs to 1:2 scale. 2- calculate gradients. 3- calculate magnitudes & orientation
+import pickle
 import uuid
 import cv2
 import os
 from skimage.feature import hog
-from sklearn.externals import joblib
+import joblib
+import numpy as np
+from torch import int32
 
 data_src = './data'
 path_features = './data/features'
 
 categories = ['0', '1']
 data = []
+data_features = []
 
 def resize_images():
     for category in categories:
@@ -34,10 +38,18 @@ def extract_features():
 
     for image,label in data:
         fd =hog(image, orientations=9, pixels_per_cell=(9, 9), cells_per_block=(2, 2), block_norm='L1', visualize=False, transform_sqrt=False, feature_vector=True)
-        fd_name = str(label) + '-' + uuid.uuid4().hex + ".feat"
+        fd_name = str(label) + '-' + uuid.uuid4().hex + ".npy"
         fd_path = os.path.join(path_features, fd_name)
         joblib.dump(fd, fd_path)
+        data_features.append([fd,label])
+
+    pick_in = open('./data/picked.pickle', 'wb')
+    pickle.dump(data_features, pick_in)
+    pick_in.close()
+    print(len(data_features))
+
     print("HOG features saved in {}".format(path_features))
+
 
 
 if __name__=='__main__':
