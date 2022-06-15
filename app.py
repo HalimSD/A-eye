46dd56271e23efec4ce0faf29d0d1b4224dcd6ef
Rahaf <rahafffrahaf97@gmail.com>
2022-06-15 14:54:16 +0200

    styling and show data in ui :)

--- app.py
+++ app.py
@@ -2,22 +2,9 @@ import time
 from flask import Flask, render_template,request,flash,redirect,url_for,session
 import sqlite3
 import argparse
-from a_eye import Image, caption_live, caption_from_device, generate_caption, torch, load_checkpoint, ClipCaptionModel,os
+from a_eye import Image, caption_live, caption_from_device, generate_caption, load_checkpoint,os
 
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--project', dest='project', action="store_true")
-# parser.add_argument('--live', default='l')
-# parser.add_argument('--pretrained', dest='pretrained', action="store_true")
-# parser.add_argument('--ClipCaptionModel', dest='ccm', action="store_true")
-# parser.add_argument('--prefix_length', type=int, default=10)
-# parser.add_argument('--clip_length', type=int, default=10)
-# parser.add_argument('--prefix_size', type=int, default=512)
-# parser.add_argument('--coco', dest='coco', action="store_true")
-# parser.add_argument('--conceptual', dest='conceptual', action="store_true")
-# parser.add_argument('--transformer', dest='transformer', action="store_true")
-
-# args = parser.parse_args()
 app = Flask(__name__)
 app.secret_key="123"
 
@@ -33,32 +20,39 @@ def index():
 def pretrained():
     model = load_checkpoint(argparse.Namespace(ccm=False, clip_length=10, coco=False, conceptual=True, live='l', prefix_length=10, prefix_size=512, pretrained=True, project=False, transformer=False))
 #   caption_from_device(m)
-    start_time = time.time()
     test_data_path = os.path.join(os.getcwd(),'./data/test')
     image_paths = [os.path.join(test_data_path, name) for name in os.listdir(test_data_path) if name[-4] == '.']
     img_list = [Image.open(image) for image in image_paths]  
     res = ""
+    results = ""
     for image in img_list:
             caption = generate_caption(image, model )
-            res = res+caption
-            # print("caaaaaaaaaaaaap",caption)
-            # print("resssssss",res)
-            # return caption
-    # print("--- %s overal time ---" % (time.time() - start_time))
-    # print("traaaaaaaaaassssssssssssssssss", res)
-    # print("captionnnnnnnnnnnnnnnnnnnnnnnnnnnn",caption)  
-    return res
+            res = res+caption+'\n'
+            results = res.replace('\n', '<br>')
+    return render_template('view.html', result=results)
+
 @app.route('/project/')
 def project():
-  m = load_checkpoint(argparse.Namespace(ccm=False, clip_length=10, coco=False, conceptual=True, live='l', prefix_length=10, prefix_size=512, pretrained=False, project=True, transformer=False))
-  caption_from_device(m)
-  return 'project conceptual'
+    model = load_checkpoint(argparse.Namespace(ccm=False, clip_length=10, coco=False, conceptual=True, live='l', prefix_length=10, prefix_size=512, pretrained=False, project=True, transformer=False))
+#   caption_from_device(m)
+    test_data_path = os.path.join(os.getcwd(),'./data/test')
+    image_paths = [os.path.join(test_data_path, name) for name in os.listdir(test_data_path) if name[-4] == '.']
+    img_list = [Image.open(image) for image in image_paths]  
+    res = ""
+    results = ""
+    for image in img_list:
+            caption = generate_caption(image, model )
+            res = res+caption+'\n'
+            results = res.replace('\n', '<br>')
+    return render_template('view.html', result=results)
+    
+#   return 'project conceptual'
 
 # Run a_eye.py --project --conceptual
 @app.route('/live/')
 def live ():
-  m = load_checkpoint(argparse.Namespace(ccm=False, clip_length=10, coco=False, conceptual=True, live='l', prefix_length=10, prefix_size=512, pretrained=False, project=True, transformer=False))
-  caption_live(m)
+  model = load_checkpoint(argparse.Namespace(ccm=False, clip_length=10, coco=False, conceptual=True, live='l', prefix_length=10, prefix_size=512, pretrained=False, project=True, transformer=False))
+  caption_live(model)
   return 'Live conceptual'
 
 @app.route('/login',methods=["GET","POST"])
