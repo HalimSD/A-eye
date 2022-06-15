324a323d2942462d346a0e6a288dd5cb7c7e7a68
Rahaf <rahafffrahaf97@gmail.com>
2022-06-15 12:03:57 +0200

    view data from console to ui

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
+import time
 from flask import Flask, render_template,request,flash,redirect,url_for,session
 import sqlite3
 import argparse
-from a_eye import WEIGHTS_PATHS, caption_live,device, caption_from_device, torch, load_checkpoint, ClipCaptionModel,os
+from a_eye import Image, caption_live, caption_from_device, generate_caption, torch, load_checkpoint, ClipCaptionModel,os
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--project', dest='project', action="store_true")
-parser.add_argument('--live', default='l')
-parser.add_argument('--pretrained', dest='pretrained', action="store_true")
-parser.add_argument('--ClipCaptionModel', dest='ccm', action="store_true")
-parser.add_argument('--prefix_length', type=int, default=10)
-parser.add_argument('--clip_length', type=int, default=10)
-parser.add_argument('--prefix_size', type=int, default=512)
-parser.add_argument('--coco', dest='coco', action="store_true")
-parser.add_argument('--conceptual', dest='conceptual', action="store_true")
-parser.add_argument('--transformer', dest='transformer', action="store_true")
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--project', dest='project', action="store_true")
+# parser.add_argument('--live', default='l')
+# parser.add_argument('--pretrained', dest='pretrained', action="store_true")
+# parser.add_argument('--ClipCaptionModel', dest='ccm', action="store_true")
+# parser.add_argument('--prefix_length', type=int, default=10)
+# parser.add_argument('--clip_length', type=int, default=10)
+# parser.add_argument('--prefix_size', type=int, default=512)
+# parser.add_argument('--coco', dest='coco', action="store_true")
+# parser.add_argument('--conceptual', dest='conceptual', action="store_true")
+# parser.add_argument('--transformer', dest='transformer', action="store_true")
 
-args = parser.parse_args()
+# args = parser.parse_args()
 app = Flask(__name__)
 app.secret_key="123"
 
@@ -30,16 +31,23 @@ def index():
 # Run a_eye.py --pretrained --conceptual
 @app.route('/pretrained/')
 def pretrained():
-  m = load_checkpoint(argparse.Namespace(ccm=False, clip_length=10, coco=False, conceptual=True, live='l', prefix_length=10, prefix_size=512, pretrained=True, project=False, transformer=False))
-  caption_from_device(m)
-#   print('Conceptual pretrained model')
-#   model_path = os.path.join(WEIGHTS_PATHS.get('pretrained_conceptual'), 'conceptual_weights.pt')
-#   checkpoint = torch.load(model_path, map_location=device)
-#   model = ClipCaptionModel(prefix_length = 10, prefix_size = 512)
-#   model.load_state_dict(checkpoint)
-#   model.eval()
-#   model.to(device=device)
-  return 'Conceptual pretrained model'
+    model = load_checkpoint(argparse.Namespace(ccm=False, clip_length=10, coco=False, conceptual=True, live='l', prefix_length=10, prefix_size=512, pretrained=True, project=False, transformer=False))
+#   caption_from_device(m)
+    start_time = time.time()
+    test_data_path = os.path.join(os.getcwd(),'./data/test')
+    image_paths = [os.path.join(test_data_path, name) for name in os.listdir(test_data_path) if name[-4] == '.']
+    img_list = [Image.open(image) for image in image_paths]  
+    res = ""
+    for image in img_list:
+            caption = generate_caption(image, model )
+            res = res+caption
+            # print("caaaaaaaaaaaaap",caption)
+            # print("resssssss",res)
+            # return caption
+    # print("--- %s overal time ---" % (time.time() - start_time))
+    # print("traaaaaaaaaassssssssssssssssss", res)
+    # print("captionnnnnnnnnnnnnnnnnnnnnnnnnnnn",caption)  
+    return res
 @app.route('/project/')
 def project():
   m = load_checkpoint(argparse.Namespace(ccm=False, clip_length=10, coco=False, conceptual=True, live='l', prefix_length=10, prefix_size=512, pretrained=False, project=True, transformer=False))
